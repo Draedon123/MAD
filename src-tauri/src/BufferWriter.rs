e9c97d5e371f889c02bf32b4f79320b9017d0a20
Draedon <draedon123@gmail.com>
2026-01-14 03:20:33 +1000

    Use macro rules in BufferWriter.rs

--- src-tauri/src/BufferWriter.rs
+++ src-tauri/src/BufferWriter.rs
@@ -1,52 +1,22 @@
+use paste::paste;
+
 pub struct BufferWriter {
   pub buffer: Vec<u8>,
 }
 
-pub trait EndianRead {
-  type Array: AsRef<[u8]>;
-  fn to_le_bytes(self) -> Self::Array;
-  fn from_le_bytes(bytes: Self::Array) -> Self;
-}
+macro_rules! impl_write_number (( $($int:ident),* ) => {
+    $(
+      paste! {
+        impl BufferWriter {
+          pub fn [<write_ $int>](&mut self, value: $int) {
+            self.buffer.extend_from_slice(&value.to_le_bytes());
+          }
+        }
+      }
+    )*
+});
 
-impl EndianRead for u8 {
-  type Array = [u8; 1];
-  fn to_le_bytes(self) -> Self::Array {
-    self.to_le_bytes()
-  }
-  fn from_le_bytes(bytes: Self::Array) -> Self {
-    Self::from_le_bytes(bytes)
-  }
-}
-
-impl EndianRead for u16 {
-  type Array = [u8; 2];
-  fn to_le_bytes(self) -> Self::Array {
-    self.to_le_bytes()
-  }
-  fn from_le_bytes(bytes: Self::Array) -> Self {
-    Self::from_le_bytes(bytes)
-  }
-}
-
-impl EndianRead for u32 {
-  type Array = [u8; 4];
-  fn to_le_bytes(self) -> Self::Array {
-    self.to_le_bytes()
-  }
-  fn from_le_bytes(bytes: Self::Array) -> Self {
-    Self::from_le_bytes(bytes)
-  }
-}
-
-impl EndianRead for u64 {
-  type Array = [u8; 8];
-  fn to_le_bytes(self) -> Self::Array {
-    self.to_le_bytes()
-  }
-  fn from_le_bytes(bytes: Self::Array) -> Self {
-    Self::from_le_bytes(bytes)
-  }
-}
+impl_write_number!(u8, u16, u32, u64);
 
 impl BufferWriter {
   pub fn new(byte_length: usize) -> Self {
@@ -57,9 +27,5 @@ impl BufferWriter {
 
   pub fn write_u8_array(&mut self, u8_array: &mut Vec<u8>) {
     self.buffer.append(u8_array);
-  }
-
-  pub fn write(&mut self, value: impl EndianRead) {
-    self.buffer.extend_from_slice(value.to_le_bytes().as_ref());
   }
 }
